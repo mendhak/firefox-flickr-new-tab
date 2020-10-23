7ba29aded0965065a1e8a8c9687851fe4edc5507
mendhak <mendhak@users.noreply.github.com>
2020-10-23 09:45:51 +0100

    Construct photo link with dataset owner

--- js/initialise.js
+++ js/initialise.js
@@ -1,20 +1,24 @@
 
 
 getImage = function() {
-	api_key='ce627966d2544e939f0306fcbfd919ce';
-	set_id='72157716222153076';
-	
-	
-	var flickrURL =  'https://api.flickr.com/services/rest/?&method=flickr.photosets.getPhotos&extras=url_k,url_3k,url_4k,url_q,url_o&api_key=' + api_key + '&photoset_id=' + set_id + '&per_page=100' + '&sort=date-posted-desc&format=json&nojsoncallback=1';
-	
-	fetch(flickrURL)
-	  .then(response => response.json())
-	  .then(data => {
-		  let img = data.photoset.photo[Math.floor(Math.random() * data.photoset.photo.length)];
-		  let photoLink = "https://www.flickr.com/photos/" + (data.photoset ? data.photoset.owner : item.owner) + "/" + img.id + "/";
-		  document.getElementById("theimage").setAttribute("src",img.url_k);
-		  document.getElementById("thelink").setAttribute("href",photoLink);
-	  }); 
+    api_key='ce627966d2544e939f0306fcbfd919ce';
+    set_id='72157716222153076';
+    
+    
+    var flickrURL =  'https://api.flickr.com/services/rest/?&method=flickr.photosets.getPhotos&extras=url_k,url_3k,url_4k,url_q,url_o&api_key=' 
+                        + api_key 
+                        + '&photoset_id=' + set_id 
+                        + '&per_page=100' 
+                        + '&sort=date-posted-desc&format=json&nojsoncallback=1';
+        
+    fetch(flickrURL)
+      .then(response => response.json())
+      .then(data => {
+          let img = data.photoset.photo[Math.floor(Math.random() * data.photoset.photo.length)];
+          let photoLink = "https://www.flickr.com/photos/" + data.photoset.owner  + "/" + img.id + "/";
+          document.getElementById("theimage").setAttribute("src",img.url_k);
+          document.getElementById("thelink").setAttribute("href",photoLink);
+      }); 
 
 }
 getImage();
